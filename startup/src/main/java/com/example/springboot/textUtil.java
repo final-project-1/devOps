a00696432bb4b2c5b0fe06b4d6f1ec9946027996
אילה שפירא <212064943@mbydomain>
2021-11-02 20:00:01 +0200

    func

---
+++ startup/src/main/java/com/example/springboot/textUtil.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class TextUtil {
+
+	
+	public static String getText() {
+		return "Ayala Shapira!!!!!!!!!!!!!";
+	}
+
+}
